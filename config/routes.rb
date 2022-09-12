9e184a4bafbf2050725ccdbd960ff5587c7815bc
Liam Hayden <65085447+LiamHayden@users.noreply.github.com>
2022-09-12 15:30:50 +0100

    Add/setup post (#2)
    * Add devise gem and config
    
    * Add post model
    
    * Add posts controller
    
    * Add resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "post#index"
+  
+  root "posts#index"
+  resources :posts, only: [:new, :create, :index]
 end
