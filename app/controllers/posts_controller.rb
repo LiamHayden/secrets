ae705cc103cf705ece29f14939511fb086f34206
Liam Hayden <l.hayden67@outlook.com>
2022-09-12 18:16:14 +0100

    Add create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
-  before_action :user_signed_in?, only: [:new, :create]
+  # before_action :user_signed_in?, only: [:new, :create]
 
   def new
     @post = Post.new
   end
+
+	def create
+		@post = @user.build_post(post_params)
+
+		if @post.save
+			redirect_to :index
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
 end
