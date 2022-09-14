a27695743d527722945f361d6fe050d171b45163
Liam Hayden <l.hayden67@outlook.com>
2022-09-14 16:45:45 +0100

    Uncomment and fix before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # before_action :user_signed_in?, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def new
     @post = Post.new
